b8b0c26d5bda68ad7e9240874cd109e1c4f817d0
Ismail Noori <ismailafghan1953@gmail.com>
2024-07-20 22:44:14 +0500

    message

---
+++ exer.js
@@ -0,0 +1,10 @@
+//String finding segments
+
+//Problem No 1
+
+var sentence="The quick brown fox jumps over the lazy dog.";
+
+var isWordFound =sentence.indexOf("fox");
+
+console.log(isWordFound);
+
